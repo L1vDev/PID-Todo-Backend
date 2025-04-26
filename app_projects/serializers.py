156bdaac195b088ada92162bdb227b8946b3611e
L1vDev <livdev.rguez@gmail.com>
2025-04-26 14:09:03 -0400

    v0.8 add-due-days

--- app_projects/serializers.py
+++ app_projects/serializers.py
@@ -5,9 +5,10 @@ from django.utils import timezone
 class ProjectSerializer(serializers.ModelSerializer):
     completed_tasks = serializers.SerializerMethodField()
     total_tasks = serializers.SerializerMethodField()
+    due_days = serializers.SerializerMethodField()
     class Meta:
         model = Project
-        fields = ['id', 'user', 'name', 'description', 'created_at', 'finished_at', 'end_date', 'status', 'updated_at','favorite','completed_tasks','total_tasks']
+        fields = ['id', 'user', 'name', 'description', 'created_at', 'finished_at', 'end_date', 'status', 'updated_at','favorite','completed_tasks','total_tasks','due_days']
         read_only_fields = ['id', 'created_at','updated_at']
         extra_kwargs = {
             'user': {'write_only': True}
@@ -18,6 +19,12 @@ class ProjectSerializer(serializers.ModelSerializer):
     
     def get_total_tasks(self, obj):
         return obj.tasks.count()
+    
+    def get_due_days(self, obj):
+        if obj.end_date:
+            delta = obj.end_date - timezone.now().date()
+            return delta.days
+        return None
 
     def validate_end_date(self, value):
         if value <= timezone.now().date():
